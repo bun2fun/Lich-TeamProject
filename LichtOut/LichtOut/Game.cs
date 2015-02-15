3c45915b91df9bcc3f510d4ceda9a5a162f41cd9
FunnyBunny <me.funnybunny2@gmail.com>
2015-02-15 13:12:57 +0200

    commit method AllLightsAreOff

--- LichtOut/LichtOut/Game.cs
+++ LichtOut/LichtOut/Game.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿u*/sing System;****************/
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,20 @@ namespace LichtOut
         //create matrix of tiles
         //print buttons depending on mode
         //method to check if all lights are off
+
+        public bool AllLightsAreOff(LightTile[,] tiles)
+        {
+            for (int row = 0; row < tiles.GetLength(0); row++)
+            {
+                for (int col = 0; col < tiles.GetLength(1); col++)
+                {
+                   if (tiles[row, col].On)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
     }
 }
